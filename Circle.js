2f827d985bae82c38ae4b78fe9106d6fc66de1e9
MuhammadNaeem <muhammad.naeemraj@gmai.com>
2022-12-07 12:20:45 +0500

    Second commit with Simple Node Application

---
+++ Circle.js
@@ -0,0 +1,11 @@
+class Circle {
+  area(rad) {
+    return Math.PI * rad * rad;
+  }
+
+  cir(rad) {
+    return 2 * Math.PI * rad;
+  }
+}
+
+module.exports = Circle;
--- index.js
+++ index.js
@@ -1,8 +1,28 @@
-const http = require("http");
+// const { area, cir } = require("./circle");
+const Circle = require("./Circle");
 
-const server = http.createServer((req, res) => {
-    res.write("Hi welcome NodeJS nice to see you here!");
-    res.end()
-});
+const circle = new Circle();
 
-server.listen(3000);
+// circle.area();
+let a,
+  c,
+  rad = 2;
+choice = 1;
+if (rad > 0 && choice == 0) {
+  a = circle.area(rad);
+  console.log(`The area of circle is ${a}.`);
+} else if (rad > 0 && choice == 1) {
+  c = circle.cir(rad);
+  console.log(`The circumference of circle is ${c}.`);
+} else {
+  console.log(`Please enter valid value.`);
+}
+
+// const http = require("http");
+
+// const server = http.createServer((req, res) => {
+//     res.write("Hi welcome NodeJS nice to see you here!");
+//     res.end()
+// });
+
+// server.listen(3000);
