d204cbfc7221ddd3227b67e28b5c569d82d60a4b
MuhammadNaeem <muhammad.naeemraj@gmai.com>
2022-12-12 14:23:32 +0500

    File System Module Readfile and worked with pages

--- content.js
+++ content.js
@@ -1,20 +1,30 @@
 const http = require("http");
+const fs = require("node:fs");
 
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
   if (req.url === "/" || req.url === "/home") {
     res.writeHead(200, { "Contect-Type": "text/html" });
-    res.write("./page/Home.js");
-    res.end();
+    fs.readFile("pages/home.html", "utf8", (err, data) => {
+      if (err) throw err;
+      res.write(data);
+      res.end();
+    });
   } else if (req.url === "/about") {
     res.writeHead(200, { "Contect-Type": "text/html" });
-    res.write("<h1>Hi i'm AboutUs Page!</h1>");
-    res.end();
+    fs.readFile("pages/About.html", "utf8", (err, data) => {
+      if (err) throw err;
+      res.write(data);
+      res.end();
+    });
   } else {
     res.writeHead(404, { "Contect-Type": "text/html" });
-    res.write("<h1>Page! not fount!</h1>");
-    res.end();
+    fs.readFile("pages/Error.html", "utf8", (err, data) => {
+      if (err) throw err;
+      res.write(data);
+      res.end();
+    });
   }
 });
 
